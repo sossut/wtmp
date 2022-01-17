49357026ebf4dfbed8ba561fd3d2f52f646c8a34
sossut <mikki.alakari@elisanet.fi>
2022-01-17 12:12:02 +0200

    remove hc list

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 
-const coursesEn = ["Hamburger, cream sauce and poiled potates",
-                "Goan style fish curry and whole grain rice",
-                "Vegan Chili sin carne and whole grain rice",
-                "Broccoli puree soup, side salad with two napas",
-                "Lunch baguette with BBQ-turkey filling",
-                "Cheese / Chicken / Vege / Halloum burger and french fries"];
-const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-                "Goalaista kalacurrya ja täysjyväriisiä",
-                "vegaani Chili sin carne ja täysjyväriisi",
-                "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-                "Lunch baguette with BBQ-turkey filling",
-                "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+
 
 import LunchMenu from './assets/lunchmenu.json';
 console.log('menu object', LunchMenu.courses);
