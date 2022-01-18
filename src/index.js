cef5d1912a43d636778f771a92e016b332a5eacd
sossut <mikki.alakari@elisanet.fi>
2022-01-18 18:57:00 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -9,9 +9,46 @@ const menu = [
 ];
 
 const validator = (string) => {
-
+  const pattern = /^[A-ZÅÄÖ]+[\wåöä \(\)\/\,\"\-\']{3,63}$/;
+  return pattern.test(string);
 };
 
 const parser = (array) => {
-
+  for (const item of array) {
+    console.log('validator',validator(item.name));
+  }
 };
+parser(menu);
+
+const priceSorter = (array) => {
+  array.sort((a,b) => a.price - b.price);
+  console.log('price sorting',array);
+};
+priceSorter(menu);
+
+const priceFilter = (array) =>{
+  const cheapFood = array.filter((food) => {
+
+    return food.price < 5;
+  });
+  console.log('price filter',cheapFood);
+};
+priceFilter(menu);
+
+const raisePrices = (array, amount) => {
+  const procent = (amount / 100 + 1);
+  const newArr = array.map((food) => {
+    return {
+      name: food.name,
+      price: (food.price * procent).toFixed(2),
+    };
+  });
+  console.log('raised prices',newArr);
+};
+raisePrices(menu, 15);
+
+const totalCost = (array) => {
+  const cost = array.reduce((a,b) => ({price: a.price + b.price}));
+  console.log('total cost',cost);
+};
+totalCost(menu);
