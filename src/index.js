30f2e886d8b137f88b2fd02122d658192e2b95b2
sossut <mikki.alakari@elisanet.fi>
2022-01-18 15:01:45 +0200

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('Hello console!');
+
+const menu = [
+  {name: 'Lingonberry jam', price: 4.00},
+  {name: 'Mushroom and bean casserole', price: 5.50},
+  {name: 'Chili-flavoured wheat', price: 3.00},
+  {name: 'Vegetarian soup', price: 4.80},
+  {name: 'Pureed root vegetable soup with smoked cheese', price: 8.00}
+];
+
+const validator = (string) => {
+
+};
+
+const parser = (array) => {
+
+};
